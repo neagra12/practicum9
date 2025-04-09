b34c3ddb91671659719910cc1beaa15ae03e62bf
Neeha Agrawal IUB <neagra@iu.edu>
2025-04-08 22:58:09 -0400

    Practicum9

--- scripts/push-to-ghcr.sh
+++ scripts/push-to-ghcr.sh
@@ -3,6 +3,6 @@ set -u
 : "${VERSION:=v1.0.0}"
 
 
-echo "${secrets.ACCESS_TOKEN}" | docker login ghcr.io -u krisha34 --password-stdin
+echo "${secrets.ACCESS_TOKEN}" | docker login ghcr.io -u neagra12 --password-stdin
 
-docker push ghcr.io/krisha34/prime-service:v1.0.0
+docker push ghcr.io/neagra12/prime-service:v1.0.0
